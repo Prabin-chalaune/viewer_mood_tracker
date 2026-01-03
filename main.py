61c19cb7b9daca6759d60d20a805f2ecbab2c684
Prabin Chalaune <94175071+Prabin-chalaune@users.noreply.github.com>
2026-01-04 00:19:29 +0545

    Update app.run to use environment port

--- main.py
+++ main.py
@@ -37,5 +37,7 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True)
+    # app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True, use_reloader=False)
 
